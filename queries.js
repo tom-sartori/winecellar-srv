999f66d72a2f1b091e290bf5ef2ec0e6403837cb
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2022-03-15 19:27:54 +0100

    Fix app error : connection db.

--- queries.js
+++ queries.js
@@ -1,14 +1,20 @@
 const { client } = require('./config')
 
 const getAppellation = (request, response) => {
-    client.connect();
+    client.connect(err => {
+        if (err) {
+            console.error('connection error', err.stack)
+        }
+        else  {
+            console.log('Connected to db')
+            client.query('SELECT * FROM appellation ORDER BY 2 ASC;', (error, results) => {
+                if (error)
+                    throw error;
 
-    client.query('SELECT * FROM appellation ORDER BY 2 ASC;', (error, results) => {
-        if (error)
-            throw error;
-
-        response.status(200).json(results.rows)
-        client.end();
+                response.status(200).json(results.rows)
+                client.end();
+            });
+        }
     });
 }
 
